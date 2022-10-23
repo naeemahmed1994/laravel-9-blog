4d9108de04e96446eda7d5c62d3de8c1935883d9
naeemahmed1994 <ahmednaeemnafi@gmail.com>
2022-10-24 00:44:20 +0600

    Project Setup

---
+++ resources/views/master.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- CSRF Token -->
+     <title>{{ config('app.name', 'Laravel') }}</title>
+
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+     <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <style>
+        .background-image
+        {
+            background-image: url('images/background.jpg');
+            background-position: center center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+            height: 600px;
+        }
+    </style>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- <title>Blog</title> -->
+</head>
+<body>
+<header class="bg-gray-800 py-6">
+            <div class="container mx-auto flex justify-between items-center px-6">
+                <div>
+                    <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                    <a class="no-underline hover:underline" href="/">Home</a>
+                    <a class="no-underline hover:underline" href="/blog">Blog</a>
+                    @guest
+                        <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        @if (Route::has('register'))
+                            <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        @endif
+                    @else
+                        <span>{{ Auth::user()->name }}</span>
+
+                        <a href="{{ route('logout') }}"
+                           class="no-underline hover:underline"
+                           onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                            {{ csrf_field() }}
+                        </form>
+                    @endguest
+                </nav>
+            </div>
+        </header>
+<div>
+    @yield('content')
+</div>
+<div>
+    @include('public.footer')
+</div>
+
+</body>
+</html>
